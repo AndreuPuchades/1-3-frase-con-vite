a00003bbd4f31fd92e1e07ac8c335292ded770b6
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-09-27 16:48:05 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+import '../style.css'
+import {
+  letters,
+  words,
+  upperString,
+  titleString,
+  backwardsLetters,
+  backwardsWords,
+  palindrome,
+} from "./functions";
+
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <header>
+      <h1>Ejercicio 1.2: Frase</h1>
+    </header>
+    <p>Abre la consola para ver el resultado.</p>
+    <footer><small>Diseño Web en Entorno Cliente - Juan Segura</small></footer>
+  </div>
+`
